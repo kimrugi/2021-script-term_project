af46f4cd9af9a5690de0be55420ebefd804ec76c
kimrugi <kimrugi@gmail.com>
2021-06-02 15:14:55 +0900

    검색UI 수정
    더 자세하게 검색할 수 있도록 변경

--- MainGUI.py
+++ MainGUI.py
@@ -10,7 +10,10 @@ import show_graph
 WIDTH = 500
 HEIGHT = 800
 BUTTON_SIZE = 90
-
+SIGUN_LIST = ['가평군', '고양시', '과천시', '광명시', '광주시', '구리시', '군포시', '김포시', '남양주시',
+              '동두천시', '부천시', '성남시', '수원시', '시흥시', '안산시', '안성시', '안양시', '양평군',
+              '양주시', '여주시', '연천군', '오산시', '용인시', '의왕시', '의정부시', '이천시', '파주시',
+              '평택시', '포천시', '하남시', '화성시']
 class MainGUI:
     def show_result_frame(self):
         canvas_width = WIDTH - 10
@@ -39,10 +42,14 @@ class MainGUI:
         self.result_canvas.create_window((0, 0), window=self.actual_result_frame, anchor="nw", width=450)
         self.manager = box_manager.BoxManager(self.actual_result_frame, load_xml.library_list)
 
-    def search(self, event=0):
-        load_xml.search_and_register(Entry.get(self.to_search))
+    def search_sigun(self, event=0):
+        load_xml.search_and_register(ttk.Combobox.get(self.sigun))
+        self.to_search.delete(0,"end")
         self.show_result_frame()
-        pass
+
+    def search_name(self, event=0):
+        load_xml.search_name_and_register(Entry.get(self.to_search))
+        self.show_result_frame()
 
     def mail(self):
         pass
@@ -71,13 +78,18 @@ class MainGUI:
         # 상단 검색 프레임
         s_font = font.Font(self.window, size=17, weight='bold', family='consolas')
 
-        self.to_search = Entry(self.window, font=s_font)
-        self.to_search.bind('<Return>', self.search)
-        self.to_search.pack()
-        self.to_search.place(x=5, y=5, width=390, height=45)
+        self.sigun = ttk.Combobox(self.window, value=SIGUN_LIST, font=s_font, state="readonly")
+        self.sigun.place(x=5, y=5, width=120, height=45)
+        self.sigun.set("지역")
+        self.sigun.bind("<<ComboboxSelected>>", self.search_sigun)
 
-        self.search_button = Button(self.window, text="검색", command=self.search)
-        self.search_button.pack()
+        self.to_search = Entry(self.window, font=s_font)
+        self.to_search.bind('<Return>', self.search_name)
+        #self.to_search.pack()
+        self.to_search.place(x=130, y=5, width=270, height=45)
+
+        self.search_button = Button(self.window, text="검색", command=self.search_name)
+        #self.search_button.pack()
         self.search_button.place(x=405, y=5, width=90, height=45)
 
         # 중단 결과 캠버스
@@ -90,7 +102,7 @@ class MainGUI:
 
         # 하단 버튼 프레임 gmail 그래프 chatbot
         self.button_frame = Frame(self.window)
-        self.button_frame.pack()
+        #self.button_frame.pack()
         self.button_frame.place(x=5, y=750, width=canvas_width, height=85)
 
 
